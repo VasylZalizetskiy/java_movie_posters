3853f7e112812a73d233e035800b439aa2fe85ca
vasyl.zalizetskyi <vasyl.zalizetskyi@gmail.com>
2018-05-30 13:49:58 +0300

    optimize imports in SwaggerConfig

--- webapp_module/src/main/java/ua/pp/movie_posters/webapp/configs/SwaggerConfig.java
+++ webapp_module/src/main/java/ua/pp/movie_posters/webapp/configs/SwaggerConfig.java
@@ -1,23 +1,16 @@
 package ua.pp.movie_posters.webapp.configs;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
@@ -56,4 +49,3 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
     }
 
 }
-
